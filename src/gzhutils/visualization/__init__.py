e182a04c4d9837d7ddc71135b8fc3575297eee9e
rkzhai <rkzhai7@gmail.com>
2023-12-01 12:51:01 +0800

    feat: compacter dep error message of importing vis

--- src/gzhutils/visualization/__init__.py
+++ src/gzhutils/visualization/__init__.py
@@ -3,11 +3,17 @@ try:
     import matplotlib as _
     import numpy as _
 except ModuleNotFoundError:
+    # By explicitly setting the __cause__ of the exception to None,
+    # the __suppress_context__ attribute of the exception will be set to True.
+    # So although the default value of __cause__ is None, we still need to 
+    # explicitly set it to None to avoid the 
+    # "during handling..." traceback message.
+    # See https://stackoverflow.com/a/24752607/13285709 for more details.
     raise ModuleNotFoundError(
         "matplotlib and numpy are required for visualization. "
         "Try `pip install gzhutils[vis]`, which will include the dependencies "
         "needed by visualization."
-    )
+    ) from None
 
 from .draw_funcs import (
     plot_one, plot_multiple, plot_consecutive,
