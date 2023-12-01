de2aec507ca410c5baa8f18a7c0b6f069a0882dd
rkzhai <rkzhai7@gmail.com>
2023-12-01 12:06:10 +0800

    make numpy and matplotlib optional dep for vis.

--- src/gzhutils/visualization/__init__.py
+++ src/gzhutils/visualization/__init__.py
@@ -1,3 +1,14 @@
+# verify the installation of optional dependencies for VIS
+try:
+    import matplotlib as _
+    import numpy as _
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "matplotlib and numpy are required for visualization. "
+        "Try `pip install gzhutils[vis]`, which will include the dependencies "
+        "needed by visualization."
+    )
+
 from .draw_funcs import (
     plot_one, plot_multiple, plot_consecutive,
     scatter_one, scatter_multiple, scatter_consecutive,
